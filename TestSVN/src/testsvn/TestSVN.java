855d9f48ecc897aac6dc6b07a79fbd197e4db87f
Breno Brezinski <breno.b@me.com>
2016-01-20 16:54:28 +0000

    Added output msg.
    git-svn-id: https://vapp2511.ict.sait.ca/svn/CapstoneTeam17@6 c191f1a4-1d0d-8d48-a00e-eb7b87333d7b

--- TestSVN/src/testsvn/TestSVN.java
+++ TestSVN/src/testsvn/TestSVN.java
@@ -16,6 +16,7 @@ public class TestSVN {
        */
       public static void main(String[] args) {
             // TODO code application logic here
+            System.out.println("Something");
       }
       
 }
