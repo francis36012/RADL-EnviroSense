c108cab260abded0b8eddc18da9b27c8a37b96e2
Daniel Chau <danjchau@hotmail.com>
2016-02-23 21:02:09 +0000

    Report Controller Updated
    git-svn-id: https://vapp2511.ict.sait.ca/svn/CapstoneTeam17@96 c191f1a4-1d0d-8d48-a00e-eb7b87333d7b

--- envirosense/src/main/java/envirosense/controller/api/ReportController.java
+++ envirosense/src/main/java/envirosense/controller/api/ReportController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Daniel Chau
  */
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/report")
 public class ReportController {
 
     @Autowired
