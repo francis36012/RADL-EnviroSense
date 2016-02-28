f7dbeaf717d64d6ce93709f7daa51690af8eb7b5
Daniel Chau <danjchau@hotmail.com>
2016-02-28 22:43:48 +0000

    Additional Documentation in Controller Classes
    git-svn-id: https://vapp2511.ict.sait.ca/svn/CapstoneTeam17@104 c191f1a4-1d0d-8d48-a00e-eb7b87333d7b

--- envirosense/src/main/java/envirosense/controller/HomeController.java
+++ envirosense/src/main/java/envirosense/controller/HomeController.java
@@ -1,15 +1,24 @@
 package envirosense.controller;
 
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+/**
+ * Controllers for mapping the home page of the application
+ *
+ * @author Daniel Chau
+ */
 @Controller
 public class HomeController {
-	@RequestMapping(value = {"/", "/index", "/home"}, method = RequestMethod.GET)
-	public ModelAndView homePage() {
-		return new ModelAndView("index");
-	}
+
+    /**
+     * Directs to the home page view using the specified mapping
+     * @return the home page view
+     */
+    @RequestMapping(value = {"/", "/index", "/home"}, method = RequestMethod.GET)
+    public ModelAndView homePage() {
+        return new ModelAndView("index");
+    }
 }
--- envirosense/src/main/java/envirosense/controller/LoginController.java
+++ envirosense/src/main/java/envirosense/controller/LoginController.java
@@ -1,20 +1,25 @@
 package envirosense.controller;
 
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
+ * Controller to map the login screen for the application
+ *
  * @author Daniel Chau
  */
 @Controller
 public class LoginController {
 
-	@RequestMapping(value = "/login", method = RequestMethod.GET)
-	public ModelAndView login() {
-		return new ModelAndView("login");
-	}
+    /**
+     * Directs to the login view using the /login mapping
+     * @return the login view
+     */
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public ModelAndView login() {
+        return new ModelAndView("login");
+    }
 
 }
