0e69906d9af6172727e809d1f3b60f4b65d6874b
Breno Brezinski <breno.b@me.com>
2016-03-15 23:24:38 +0000

    Main script updated
    git-svn-id: https://vapp2511.ict.sait.ca/svn/CapstoneTeam17@142 c191f1a4-1d0d-8d48-a00e-eb7b87333d7b

--- EnvirosenseSensor/mainScript.sh
+++ EnvirosenseSensor/mainScript.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 sleep 10
 node /home/pi/Desktop/EnvirosenseSensor/Server/APIserver.js & 
-sleep 7
+sleep 10
 python /home/pi/Desktop/EnvirosenseSensor/libs/Python/motion.py &
 python /home/pi/Desktop/EnvirosenseSensor/libs/Python/door.py &
 /home/pi/Desktop/EnvirosenseSensor/libs/C/temphum
