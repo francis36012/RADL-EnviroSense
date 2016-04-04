30740ce4102543c9870bee9fd5346a12f9ce862b
Jediah Dizon <JediahJosuah@gmail.com>
2016-04-04 23:40:13 +0000

    Update. - Events now submits CSRF Token as a header.
    git-svn-id: https://vapp2511.ict.sait.ca/svn/CapstoneTeam17@175 c191f1a4-1d0d-8d48-a00e-eb7b87333d7b

--- envirosense/src/main/webapp/static/js/ajax/events/dataByEvents.js
+++ envirosense/src/main/webapp/static/js/ajax/events/dataByEvents.js
@@ -2,9 +2,9 @@ function setDataByEventId(formElement) {
 	var stateValue = (formElement.state.value === "true") ? "enable" : "disable";
 	var eventId = formElement.eventId.value;
 	var csrfProtection = formElement.csrfProtection;
-	var requestParameters = csrfProtection.name + "=" + csrfProtection.value;
 	
 	var xmlHttp = new XMLHttpRequest();
-	xmlHttp.open("POST", "/envirosense/api/event/" + stateValue + "/" + eventId + "?" + requestParameters, true);
+	xmlHttp.open("POST", "/envirosense/api/event/" + stateValue + "/" + eventId, true);
+	xmlHttp.setRequestHeader("X-CSRF-TOKEN", csrfProtection.value);
 	xmlHttp.send();
 }
