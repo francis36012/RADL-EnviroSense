cf2fba8b235e1ea7bf66a798776fbba4fb6c8d87
Jediah Dizon <JediahJosuah@gmail.com>
2016-04-04 01:42:19 +0000

    Forgot to commit last night. - Report page date bug fix. Report page "No Google Charts connectivity" display message now includes raw data.
    Events endable/disable ajax call now functional, but not working. (Internal Server Error.)
    -
    Working on "Manage Users" functionalities.
    Working on Live Data visual aesthetics.
    
    git-svn-id: https://vapp2511.ict.sait.ca/svn/CapstoneTeam17@172 c191f1a4-1d0d-8d48-a00e-eb7b87333d7b

--- envirosense/src/main/webapp/static/js/ajax/events/dataByEvents.js
+++ envirosense/src/main/webapp/static/js/ajax/events/dataByEvents.js
@@ -6,8 +6,10 @@
 function setDataByEventId(formElement) {
 	var stateValue = (formElement.state.value === "true") ? "enable" : "disable";
 	var eventId = formElement.eventId.value;
+	var csrfProtection = formElement.csrfProtection;
+	var requestParameters = csrfProtection.name + "=" + csrfProtection.value;
 	
 	var xmlHttp = new XMLHttpRequest();
-	xmlHttp.open("POST", "/envirosense/api/event/" + stateValue + "/" + eventId, true);
+	xmlHttp.open("POST", "/envirosense/api/event/" + stateValue + "/" + eventId + "?" + requestParameters, true);
 	xmlHttp.send();
 }
