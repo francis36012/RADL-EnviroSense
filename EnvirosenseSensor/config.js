3d1c604075af89d5cadb7278e24640c125e7607e
Breno Brezinski <breno.b@me.com>
2016-04-10 18:33:55 +0000

    Fixed query to insert remove data from local DB
    git-svn-id: https://vapp2511.ict.sait.ca/svn/CapstoneTeam17@200 c191f1a4-1d0d-8d48-a00e-eb7b87333d7b

--- EnvirosenseSensor/config.js
+++ EnvirosenseSensor/config.js
@@ -72,8 +72,7 @@ config.DB.PASSWORD = 'password';
 
 config.DB.INSERT_SENSOR_DATA =      "INSERT INTO %s SET sensor_id = %s, data = %s, timestamp = STR_TO_DATE(\'%s\','%Y %m %d %H %i %s')";
 config.DB.INSERT_SENSOR_DATA_SIMP = "INSERT INTO %s SET sensor_id = %s, data = %s, timestamp = STR_TO_DATE(\'%s\','%a %b %d %Y %H:%i:%s GMT-0700 (MST)')";
-config.DB.REMOVE_SENSOR_DATA = "DELETE FROM %s WHERE sensor_id = %s AND timestamp = STR_TO_DATE(\'%s\','%a %b %d %Y %H:%i:%s GMT-0700 (MST)')";
+config.DB.REMOVE_SENSOR_DATA = "DELETE FROM %s WHERE sensor_id = %s AND timestamp = STR_TO_DATE(\'%s\','%a %b %d %Y %H:%i:%s')";
 config.DB.GET_LOCAL_SENSOR_DATA = 'SELECT * FROM %s';
-//config.DB.GET_LOCAL_SENSORS = 'SELECT * FROM sensor WHERE room_id = ' + config.THIS_PI.ROOMID;
 
 module.exports = config;
