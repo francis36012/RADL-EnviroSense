7f35c4a0ed0d24ae3e8b17bc783ea87cdbb7c913
Francis Agayapong <francisagyapong2@gmail.com>
2016-04-29 12:06:50 -0600

    Remove test projects pulled from svn repository

--- EnvirosenseBeta/src/main/java/com/envirosense/springsecurity/configuration/SecurityWebApplicationInitializer.java
+++
@@ -1,7 +0,0 @@
-package com.envirosense.springsecurity.configuration;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-
-}
